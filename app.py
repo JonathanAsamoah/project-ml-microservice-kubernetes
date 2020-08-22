8205d82941033d2d99becffa13e72096eb0524b7
jonathan.asamoah <jonathan.asamoah@dataduties.de>
2020-08-22 23:08:18 +0200

    first commit

--- app.py
+++ app.py
@@ -55,16 +55,16 @@ def predict():
     
     # Logging the input payload
     json_payload = request.json
-    LOG.info(f"JSON payload: \n{json_payload}")
+    LOG.info("JSON payload: \n%s", json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+    LOG.info("Inference payload DataFrame: \n%s", inference_payload)
     # scale the input
     scaled_payload = scale(inference_payload)
-    LOG.info(f"Scaled payload DataFrame: \n{scaled_payload}")
+    LOG.info("Scaled payload DataFrame: \n%s", scaled_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    LOG.info(f"Prediction value: \n{prediction}")
+    LOG.info(f"Prediction value: \n%s", prediction)
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
